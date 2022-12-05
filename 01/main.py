b770ae6a3836cd913224e8852ce9c5a98f1b1d66
Tut-k0 <Tut_k0@protonmail.com>
2022-12-05 13:53:26 -0600

    Refactor for cleanliness.
    * Remove duplicate code
    * Refactor functions to be one line for cleanliness.

--- 01/main.py
+++ 01/main.py
@@ -1,31 +1,32 @@
-def problem_one(elf_calories: list[list[str]]) -> int:
+def problem_one(elf_calories: list[list[int]]) -> int:
     """Find the Elf carrying the most Calories, How many total Calories is
     that Elf carrying?
     """
-    elf_calories = [[int(calorie) for calorie in elf] for elf in elf_calories]
-    elf_calories_sum = [sum(calories) for calories in elf_calories]
-    return max(elf_calories_sum)
+    return max([sum(calories) for calories in elf_calories])
 
 
-def problem_two(elf_calories: list[list[str]]) -> int:
+def problem_two(elf_calories: list[list[int]]) -> int:
     """Find the top three Elves carrying the most Calories. How many Calories
     are those Elves carrying in total?
     """
-    elf_calories = [[int(calorie) for calorie in elf] for elf in elf_calories]
-    elf_calories_sum = [sum(calories) for calories in elf_calories]
-    elf_calories_sum.sort(reverse=True)
-    return sum(elf_calories_sum[:3])
+    return sum(
+        sorted([sum(calories) for calories in elf_calories], reverse=True)[:3]
+    )
 
 
 def main() -> None:
-    with open('input.txt', 'r') as f:
+    with open("input.txt", "r") as f:
         data = f.read()
-    elf_calories = [item.split('\n') for item in data.split('\n\n')]
+    elf_calories = [
+        [int(i) for i in item.split("\n") if i] for item in data.split("\n\n")
+    ]
     p1_answer = problem_one(elf_calories)
     p2_answer = problem_two(elf_calories)
-    print(f'Problem one answer is {p1_answer}. \n'
-          f'Problem two answer is {p2_answer}.')
+    print(
+        f"Problem one answer is {p1_answer}. \n"
+        f"Problem two answer is {p2_answer}."
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
