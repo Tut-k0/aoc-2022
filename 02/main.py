d15bb74a08427971d6161b453bdd6bad3d6af4fa
Tut-k0 <Tut_k0@protonmail.com>
2022-12-05 15:26:12 -0600

    Day 2, problem two.
    * Can be simplified I'm sure of it ;).
    * Probably by using a dictionary mapping rather than match statement.

--- 02/main.py
+++ 02/main.py
@@ -7,11 +7,74 @@ def problem_one(rps_groupings: list[list[str]]) -> int:
     """What would your total score be if everything goes exactly according to
     your strategy guide?
     """
-    return sum([rps_schema(group[0], group[1]) for group in rps_groupings])
+    scores = []
+    for group in rps_groupings:
+        score = 0
+        match group[1]:
+            case "X":
+                score += 1
+                if group[0] == "A":
+                    score += 3
+                elif group[0] == "B":
+                    pass
+                elif group[0] == "C":
+                    score += 6
+            case "Y":
+                score += 2
+                if group[0] == "A":
+                    score += 6
+                elif group[0] == "B":
+                    score += 3
+                elif group[0] == "C":
+                    pass
+            case "Z":
+                score += 3
+                if group[0] == "A":
+                    pass
+                elif group[0] == "B":
+                    score += 6
+                elif group[0] == "C":
+                    score += 3
+        scores.append(score)
+
+    return sum(scores)
 
 
-def problem_two():
-    pass
+# For problem 2, X means I need to lose, Y draw, and Z win.
+# 1 Point for choosing rock, 2 points for paper, 3 for scissors.
+# 0 points loss, 3 points draw, 6 points win
+def problem_two(rps_groupings: list[list[str]]):
+    """Following the Elf's instructions for the second column, what would your
+    total score be if everything goes exactly according to your strategy guide?
+    """
+    scores = []
+    for group in rps_groupings:
+        score = 0
+        match group[1]:
+            case "X":
+                if group[0] == "A":
+                    score += 3
+                elif group[0] == "B":
+                    score += 1
+                elif group[0] == "C":
+                    score += 2
+            case "Y":
+                if group[0] == "A":
+                    score += 4
+                elif group[0] == "B":
+                    score += 5
+                elif group[0] == "C":
+                    score += 6
+            case "Z":
+                if group[0] == "A":
+                    score += 8
+                elif group[0] == "B":
+                    score += 9
+                elif group[0] == "C":
+                    score += 7
+        scores.append(score)
+
+    return sum(scores)
 
 
 def main() -> None:
@@ -21,37 +84,11 @@ def main() -> None:
         [group for group in item.split(" ")] for item in data.split("\n")
     ]
     p1_answer = problem_one(rps_groupings)
-    print(p1_answer)
-
-
-def rps_schema(opponent: str, ours: str) -> int:
-    score: int = 0
-    match ours:
-        case "X":
-            score += 1
-            if opponent == "A":
-                score += 3
-            elif opponent == "B":
-                pass
-            elif opponent == "C":
-                score += 6
-        case "Y":
-            score += 2
-            if opponent == "A":
-                score += 6
-            elif opponent == "B":
-                score += 3
-            elif opponent == "C":
-                pass
-        case "Z":
-            score += 3
-            if opponent == "A":
-                pass
-            elif opponent == "B":
-                score += 6
-            elif opponent == "C":
-                score += 3
-    return score
+    p2_answer = problem_two(rps_groupings)
+    print(
+        f"Problem one answer is {p1_answer}. \n"
+        f"Problem two answer is {p2_answer}."
+    )
 
 
 if __name__ == "__main__":
